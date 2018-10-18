8824ca92e65f4902b01ab86cbaf12e71bda2977a
Evan Cushing <evdcush@protonmail.com>
2018-10-17 23:45:44 -0700

    init dunder files

---
+++ sandbox/deep_learning/__init__.py
@@ -0,0 +1,8 @@
+import deep_learning
+import deep_learning.functions
+import deep_learning.utils
+import deep_learning.layers
+import deep_learning.network
+import deep_learning.optimizers
+import deep_learning.train
+import deep_learning.initializers
